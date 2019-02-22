c9852e64cfd47b23adfc7b7d7c2a16482744c362
imverka <mpsv1992@gmail.com>
2019-02-22 01:11:15 -0500

    make it better

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
 module.exports = {
     //local Mongo DB deployment
     "URI":"mongodb://localhost/testing"
+    ///adhhdsdsadhjshdjasdhn
+    
     // "URI":"mongodb://verka:a123456@ds044989.mlab.com:44989/maninder"
 }
